cedc8e6ea6a82d74342a281dd48540fa6b01707e
StevenGann <Steven.Gann@Microchip.com>
2022-03-09 23:37:52 -0700

    Implemented simple asset loading

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include "Game/Systems/FreeCameraSystem.h"
 #include "Engine/Core/Application.cpp"
 
+#include <boost/math/quaternion.hpp>
+
 int main(int argc, char *argv[])
 {
+    boost::math::quaternion MyQuat = boost::math::quaternion<float>();
+
     Application app;
     app.setup();
 
--- src/Engine/Core/Systems/GraphicsSystem.h
+++ src/Engine/Core/Systems/GraphicsSystem.h
@@ -12,6 +12,8 @@
 class GraphicsSystem : public SystemBase
 {
     Camera3D camera;
+    Model model;
+    Texture2D texture;
 
     void OnStartup(entt::registry &registry) override
     {
@@ -19,24 +21,32 @@ class GraphicsSystem : public SystemBase
         SetTargetFPS(60);
 
         camera = {0};
-        camera.position = (Vector3){10.0f, 10.0f, 10.0f}; // Camera position
+        camera.position = (Vector3){0.0f, 10.0f, 0.0f}; // Camera position
         camera.target = (Vector3){0.0f, 0.0f, 0.0f};      // Camera looking at point
         camera.up = (Vector3){0.0f, 1.0f, 0.0f};          // Camera up vector (rotation towards target)
-        camera.fovy = 45.0f;                              // Camera field-of-view Y
+        camera.fovy = 75.0f;                              // Camera field-of-view Y
         camera.projection = CAMERA_PERSPECTIVE;           // Camera mode type
         SetCameraMode(camera, CAMERA_FIRST_PERSON);       // Set a free camera mode
         
         std::cout << "Generating scene";
-        float roidCount = 100.0f;
+        float roidCount = 1000.0f;
         for (int i = 0; i < roidCount; i++)
         {
             auto entity = registry.create();
             registry.emplace<MeshComponent>(entity);
             float x = ((rand() - rand()) / (float)RAND_MAX ) * roidCount;
-            float z = ((rand() - rand()) / (float)RAND_MAX ) * roidCount;
             float y = ((rand() - rand()) / (float)RAND_MAX ) * roidCount * 0.1f;
+            float z = ((rand() - rand()) / (float)RAND_MAX ) * roidCount;
+            
             registry.emplace<TransformComponent>(entity, x, y, z);
         }
+
+        std::cout << "Loading resources";
+        model = LoadModel("media/Asteroid/asteroid_03.obj");
+        texture = LoadTexture("media/Asteroid/asteroid_4k.jpg");
+        model.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = texture;
+
+
     }
 
     void OnUpdate(entt::registry &registry) override
@@ -55,11 +65,10 @@ class GraphicsSystem : public SystemBase
         {
             auto &transform = transformView.get<TransformComponent>(entity);
             Vector3 pos = (Vector3){transform.X, transform.Y, transform.Z};
-
-            DrawCube(pos, 2.0f, 2.0f, 2.0f, RED);
+            DrawModel(model, pos, 1.0f, WHITE);
         }
 
-        DrawGrid(100, 10.0f);
+        DrawGrid(10, 1.0f);
 
         EndMode3D();
 

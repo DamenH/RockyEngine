4328dc22cdba9adf44bd4495c24bdfad03a5d7a3
StevenGann <Steven.Gann@Microchip.com>
2022-04-10 20:58:41 -0700

    Implemented basic skybox

--- src/Engine/Core/Systems/GraphicsSystem.h
+++ src/Engine/Core/Systems/GraphicsSystem.h
@@ -36,6 +36,7 @@ class GraphicsSystem : public SystemBase
 
     inline static bool DebugGuiActive = false;
 
+    inline static Model skybox;
 
     void OnStartup(entt::registry &registry) override
     {
@@ -58,7 +59,32 @@ class GraphicsSystem : public SystemBase
 #endif
         Debug::Initialize();
         Debug::RegisterCallback("GraphicsSystem", &DebugGuiCallback);
-        Profiler::Initialize(); //ToDo: Move this initialization someplace else
+        Profiler::Initialize(); // ToDo: Move this initialization someplace else
+
+        // ====================================================================
+        Mesh cube = GenMeshCube(1.0f, 1.0f, 1.0f);
+        skybox = LoadModelFromMesh(cube);
+        skybox.materials[0].shader = LoadShader(TextFormat("../media/Shaders/skybox.vs", 330),
+                                                TextFormat("../media/Shaders/skybox.fs", 330));
+
+        SetShaderValue(skybox.materials[0].shader, GetShaderLocation(skybox.materials[0].shader, "environmentMap"), (int[1]){MATERIAL_MAP_CUBEMAP}, SHADER_UNIFORM_INT);
+        SetShaderValue(skybox.materials[0].shader, GetShaderLocation(skybox.materials[0].shader, "doGamma"), (int[1]){1}, SHADER_UNIFORM_INT);
+        SetShaderValue(skybox.materials[0].shader, GetShaderLocation(skybox.materials[0].shader, "vflipped"), (int[1]){1}, SHADER_UNIFORM_INT);
+
+        // Load cubemap shader and setup required shader locations
+        Shader shdrCubemap = LoadShader(TextFormat("../media/Shaders/cubemap.vs", 330),
+                                        TextFormat("../media/Shaders/cubemap.fs", 330));
+
+        SetShaderValue(shdrCubemap, GetShaderLocation(shdrCubemap, "equirectangularMap"), (int[1]){0}, SHADER_UNIFORM_INT);
+        Texture2D panorama;
+        panorama = LoadTexture("../media/Environment/nebula-n0.hdr");
+        // Generate cubemap (texture with 6 quads-cube-mapping) from panorama HDR texture
+        // NOTE 1: New texture is generated rendering to texture, shader calculates the sphere->cube coordinates mapping
+        // NOTE 2: It seems on some Android devices WebGL, fbo does not properly support a FLOAT-based attachment,
+        // despite texture can be successfully created.. so using PIXELFORMAT_UNCOMPRESSED_R8G8B8A8 instead of PIXELFORMAT_UNCOMPRESSED_R32G32B32A32
+        skybox.materials[0].maps[MATERIAL_MAP_CUBEMAP].texture = GenTextureCubemap(shdrCubemap, panorama, 1024, PIXELFORMAT_UNCOMPRESSED_R8G8B8A8);
+        // UnloadTexture(panorama);    // Texture not required anymore, cubemap already generated
+        // ====================================================================
     }
 
     void OnUpdate(entt::registry &registry) override
@@ -74,7 +100,7 @@ class GraphicsSystem : public SystemBase
         {
             CameraComponent cam = camerasView.get<CameraComponent>(entity);
             auto &transform = camerasView.get<TransformComponent>(entity);
-            //camera.position = transform.Translation;
+            // camera.position = transform.Translation;
             target = cam.Target;
         }
 
@@ -99,6 +125,15 @@ class GraphicsSystem : public SystemBase
             VisibilitySystem::frustum.Extract();
             VisibilitySystem::CullingCamera = camera;
         }
+
+        // ====================================================================
+        // We are inside the cube, we need to disable backface culling!
+        rlDisableBackfaceCulling();
+        rlDisableDepthMask();
+        DrawModel(skybox, (Vector3){0, 0, 0}, 1.0f, WHITE);
+        rlEnableBackfaceCulling();
+        rlEnableDepthMask();
+        // ====================================================================
 
         auto binsView = registry.view<InstanceBinComponent>();
         for (auto entity : binsView)
@@ -144,7 +179,7 @@ class GraphicsSystem : public SystemBase
 
         if (DebugGuiActive)
         {
-            Debug::Draw();            
+            Debug::Draw();
         }
 
         if (!DebugGuiActive || IsMouseButtonDown(MOUSE_BUTTON_RIGHT))
@@ -171,5 +206,94 @@ class GraphicsSystem : public SystemBase
         ImGui::Text("Triangles: %d", TriangleCount);
         ImGui::Text("FPS: %d", GetFPS());
         ImGui::Text("Draw Batches: %d", batchCount);
+    }
+
+    // Generate cubemap texture from HDR texture
+    static TextureCubemap GenTextureCubemap(Shader shader, Texture2D panorama, int size, int format)
+    {
+        TextureCubemap cubemap = {0};
+
+        rlDisableBackfaceCulling(); // Disable backface culling to render inside the cube
+
+        // STEP 1: Setup framebuffer
+        //------------------------------------------------------------------------------------------
+        unsigned int rbo = rlLoadTextureDepth(size, size, true);
+        cubemap.id = rlLoadTextureCubemap(0, size, format);
+
+        unsigned int fbo = rlLoadFramebuffer(size, size);
+        rlFramebufferAttach(fbo, rbo, RL_ATTACHMENT_DEPTH, RL_ATTACHMENT_RENDERBUFFER, 0);
+        rlFramebufferAttach(fbo, cubemap.id, RL_ATTACHMENT_COLOR_CHANNEL0, RL_ATTACHMENT_CUBEMAP_POSITIVE_X, 0);
+
+        // Check if framebuffer is complete with attachments (valid)
+        if (rlFramebufferComplete(fbo))
+            TraceLog(LOG_INFO, "FBO: [ID %i] Framebuffer object created successfully", fbo);
+        //------------------------------------------------------------------------------------------
+
+        // STEP 2: Draw to framebuffer
+        //------------------------------------------------------------------------------------------
+        // NOTE: Shader is used to convert HDR equirectangular environment map to cubemap equivalent (6 faces)
+        rlEnableShader(shader.id);
+
+        // Define projection matrix and send it to shader
+        Matrix matFboProjection = MatrixPerspective(90.0 * DEG2RAD, 1.0, RL_CULL_DISTANCE_NEAR, RL_CULL_DISTANCE_FAR);
+        rlSetUniformMatrix(shader.locs[SHADER_LOC_MATRIX_PROJECTION], matFboProjection);
+
+        // Define view matrix for every side of the cubemap
+        Matrix fboViews[6] = {
+            MatrixLookAt((Vector3){0.0f, 0.0f, 0.0f}, (Vector3){1.0f, 0.0f, 0.0f}, (Vector3){0.0f, -1.0f, 0.0f}),
+            MatrixLookAt((Vector3){0.0f, 0.0f, 0.0f}, (Vector3){-1.0f, 0.0f, 0.0f}, (Vector3){0.0f, -1.0f, 0.0f}),
+            MatrixLookAt((Vector3){0.0f, 0.0f, 0.0f}, (Vector3){0.0f, 1.0f, 0.0f}, (Vector3){0.0f, 0.0f, 1.0f}),
+            MatrixLookAt((Vector3){0.0f, 0.0f, 0.0f}, (Vector3){0.0f, -1.0f, 0.0f}, (Vector3){0.0f, 0.0f, -1.0f}),
+            MatrixLookAt((Vector3){0.0f, 0.0f, 0.0f}, (Vector3){0.0f, 0.0f, 1.0f}, (Vector3){0.0f, -1.0f, 0.0f}),
+            MatrixLookAt((Vector3){0.0f, 0.0f, 0.0f}, (Vector3){0.0f, 0.0f, -1.0f}, (Vector3){0.0f, -1.0f, 0.0f})};
+
+        rlViewport(0, 0, size, size); // Set viewport to current fbo dimensions
+
+        // Activate and enable texture for drawing to cubemap faces
+        rlActiveTextureSlot(0);
+        rlEnableTexture(panorama.id);
+
+        for (int i = 0; i < 6; i++)
+        {
+            // Set the view matrix for the current cube face
+            rlSetUniformMatrix(shader.locs[SHADER_LOC_MATRIX_VIEW], fboViews[i]);
+
+            // Select the current cubemap face attachment for the fbo
+            // WARNING: This function by default enables->attach->disables fbo!!!
+            rlFramebufferAttach(fbo, cubemap.id, RL_ATTACHMENT_COLOR_CHANNEL0, RL_ATTACHMENT_CUBEMAP_POSITIVE_X + i, 0);
+            rlEnableFramebuffer(fbo);
+
+            // Load and draw a cube, it uses the current enabled texture
+            rlClearScreenBuffers();
+            rlLoadDrawCube();
+
+            // ALTERNATIVE: Try to use internal batch system to draw the cube instead of rlLoadDrawCube
+            // for some reason this method does not work, maybe due to cube triangles definition? normals pointing out?
+            // TODO: Investigate this issue...
+            // rlSetTexture(panorama.id); // WARNING: It must be called after enabling current framebuffer if using internal batch system!
+            // rlClearScreenBuffers();
+            // DrawCubeV(Vector3Zero(), Vector3One(), WHITE);
+            // rlDrawRenderBatchActive();
+        }
+        //------------------------------------------------------------------------------------------
+
+        // STEP 3: Unload framebuffer and reset state
+        //------------------------------------------------------------------------------------------
+        rlDisableShader();        // Unbind shader
+        rlDisableTexture();       // Unbind texture
+        rlDisableFramebuffer();   // Unbind framebuffer
+        rlUnloadFramebuffer(fbo); // Unload framebuffer (and automatically attached depth texture/renderbuffer)
+
+        // Reset viewport dimensions to default
+        rlViewport(0, 0, rlGetFramebufferWidth(), rlGetFramebufferHeight());
+        rlEnableBackfaceCulling();
+        //------------------------------------------------------------------------------------------
+
+        cubemap.width = size;
+        cubemap.height = size;
+        cubemap.mipmaps = 1;
+        cubemap.format = format;
+
+        return cubemap;
     }
 };
